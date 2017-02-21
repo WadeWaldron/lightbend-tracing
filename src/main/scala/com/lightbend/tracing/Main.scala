249d020398e2558e21cb7db716de5c075a09cab0
wadewaldron <wade.waldron@boldradius.com>
2017-02-21 14:58:50 -0600

    Cleaning up some code, removing some sharding.

--- src/main/scala/com/lightbend/tracing/Main.scala
+++ src/main/scala/com/lightbend/tracing/Main.scala
@@ -5,14 +5,11 @@ import java.util.UUID
 import akka.actor.{PoisonPill, Props, ActorSystem}
 import akka.cluster.sharding.ShardRegion.MessageExtractor
 import akka.cluster.sharding.{ClusterShardingSettings, ClusterSharding}
-import akka.cluster.singleton.{ClusterSingletonManagerSettings, ClusterSingletonManager}
 import akka.pattern.ask
 import akka.util.Timeout
-import org.slf4j.MDC
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
-import scala.util.Success
 
 object Main extends App {
 
@@ -21,36 +18,40 @@ object Main extends App {
 
   implicit val timeout = Timeout(10.seconds)
 
-  val messageExtractor = new MessageExtractor {
+  //  val messageExtractor = new MessageExtractor {
+  //
+  //    override def entityId(message: Any): String = message match {
+  //      case Request.BeginRequest(requestId) => requestId.toString
+  //    }
+  //
+  //    override def shardId(message: Any): String = message match {
+  //      case Request.BeginRequest(requestId) => (Math.abs(requestId.hashCode()) % 10).toString
+  //    }
+  //
+  //    override def entityMessage(message: Any): Any = message
+  //  }
 
-    override def entityId(message: Any): String = message match {
-      case Request.BeginRequest(requestId) => requestId.toString
-    }
-
-    override def shardId(message: Any): String = message match {
-      case Request.BeginRequest(requestId) => (Math.abs(requestId.hashCode()) % 10).toString
-    }
-
-    override def entityMessage(message: Any): Any = message
-  }
-
-  system.actorOf(
-    ClusterSingletonManager.props(
-      singletonProps = Shipping.props(),
-      terminationMessage = PoisonPill,
-      settings = ClusterSingletonManagerSettings(system)),
-    name = "shipping")
+  //  system.actorOf(
+  //    ClusterSingletonManager.props(
+  //      singletonProps = Shipping.props(),
+  //      terminationMessage = PoisonPill,
+  //      settings = ClusterSingletonManagerSettings(system)),
+  //    name = "shipping")
 
 
-  val requests = ClusterSharding(system).start(
-    "request",
-    Request.props(),
-    ClusterShardingSettings(system),
-    messageExtractor
-  )
+  //  val requests = ClusterSharding(system).start(
+  //    "request",
+  //    Request.props(),
+  //    ClusterShardingSettings(system),
+  //    messageExtractor
+  //  )
 
   val futures = (1 to 10).map { _ =>
-    requests ? Request.BeginRequest(UUID.randomUUID)
+    val id = UUID.randomUUID()
+
+    val request = system.actorOf(Request.props(), id.toString)
+
+    request ? Request.BeginRequest(id)
   }
 
   Future.sequence(futures).andThen {
--- src/main/scala/com/lightbend/tracing/Request.scala
+++ src/main/scala/com/lightbend/tracing/Request.scala
@@ -5,12 +5,7 @@ import java.util.UUID
 import akka.actor.{ActorLogging, Props, ActorRef, Actor}
 import akka.cluster.sharding.ShardRegion.MessageExtractor
 import akka.cluster.sharding.{ClusterSharding, ClusterShardingSettings}
-import akka.cluster.singleton.{ClusterSingletonProxySettings, ClusterSingletonProxy}
-import akka.util.Timeout
 import org.slf4j.MDC
-
-import scala.concurrent.Await
-import scala.concurrent.duration._
 
 object Request {
   case class BeginRequest(requestId: UUID)
@@ -23,6 +18,16 @@ class Request() extends Actor with ActorLogging {
   import Request._
 
   val requestId = UUID.fromString(self.path.name)
+
+  private val messageExtractor = new MessageExtractor {
+    override def entityId(message: Any): String = message match {
+      case Shipping.ShipOrder(orderId) => orderId.toString
+    }
+    override def shardId(message: Any): String = message match {
+      case Shipping.ShipOrder(orderId) => (Math.abs(orderId.hashCode()) % 10).toString
+    }
+    override def entityMessage(message: Any): Any = message
+  }
 
   private val orderManagement = createOrderManagement()
   private val paymentProcessor = createPaymentProcessor()
@@ -37,30 +42,6 @@ class Request() extends Actor with ActorLogging {
   }
 
   protected def createShipping() = {
-//    context.actorOf(Shipping.props(), "shipping")
-//
-//    implicit val timeout = Timeout(5.seconds)
-//
-//    Await.result(context.actorSelection(self.path.toString.replace("akka://Tracing", "akka.tcp://Tracing@127.0.0.1:2551") + "/shipping").resolveOne(), 5.seconds)
-
-//    context.actorOf(
-//      ClusterSingletonProxy.props(
-//        singletonManagerPath = "/user/shipping",
-//        settings = ClusterSingletonProxySettings(context.system)),
-//      name = "shipping")
-
-
-
-    val messageExtractor = new MessageExtractor {
-      override def entityId(message: Any): String = message match {
-        case Shipping.ShipOrder(orderId) => orderId.toString
-      }
-      override def shardId(message: Any): String = message match {
-        case Shipping.ShipOrder(orderId) => (Math.abs(orderId.hashCode()) % 10).toString
-      }
-      override def entityMessage(message: Any): Any = message
-    }
-
     ClusterSharding(context.system).start(
       "shipping",
       Shipping.props(),
